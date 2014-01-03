314fde1edfcb30a90aed5221dec6ad0be35cfb51
Daniel Arnold <darnold8@gmail.com>
2014-01-02 23:18:12 -0500

    trying to debug. removing lines.

--- run.sh
+++ run.sh
@@ -6,8 +6,3 @@ if [ -n "$PRYLINES" ]; then
   fail "Pry(s) Found:\n$PRYLINES"
 fi
 
-TODOLINES=`grep -Rs --exclude-dir .git --exclude-dir log --exclude-dir tmp -n TODO: .`
-
-if [ -n "$TODOLINES" ]; then
-  fail "TODO(s) Found:\n$TODOLINES"
-fi
