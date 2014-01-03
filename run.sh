a32ed6652649b0ae0d980eb25646a3016d9e4cdb
Daniel Arnold <darnold8@gmail.com>
2014-01-02 23:29:10 -0500

    more debugging

--- run.sh
+++ run.sh
@@ -1,1 +1,15 @@
 #!/bin/sh
+
+PRYLINES=`grep -Rs --exclude-dir .git --exclude-dir log --exclude-dir tmp -n binding.pry .`
+
+if [ -n "$PRYLINES" ]; then
+  #fail "Pry(s) Found:\n$PRYLINES"
+  echo 'Broken on pry'
+fi
+
+TODOLINES=`grep -Rs --exclude-dir .git --exclude-dir log --exclude-dir tmp -n TODO: .`
+
+if [ -n "$TODOLINES" ]; then
+  # fail "TODO(s) Found:\n$TODOLINES"
+  echo 'Broken on TODO'
+fi
